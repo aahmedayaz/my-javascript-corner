e59f68e22bdf8f5a246cf54794ec146594f3ac54
Ahmed Ayaz <ahmedayaz.business@gmail.com>
2022-04-13 07:59:36 +0500

    Completed Step 00 - Console

---
+++ step-00-console/script.js
@@ -0,0 +1,23 @@
+console.log("Hello JavaScript !");
+// OUTPUT : 
+// Hello JavaScript !
+
+var name1 = "Ahmed Ayaz";
+console.log("This is" + name1);
+console.log("This is" , name1);
+// OUTPUT :
+// This isAhmed Ayaz
+// This is Ahmed Ayaz
+
+var age = 18;
+console.log("my age is "+ age);
+console.log("my age is ", age);
+// OUTPUT :
+// my age is 18 //(All is White)
+// my age is  18 //(18 is Yellow)
+
+console.log("Ayaz\tAyaz\nAyaz\tAyaz\nAyaz\tAyaz");
+// OUTPUT :
+// Ayaz    Ayaz
+// Ayaz    Ayaz
+// Ayaz    Ayaz
