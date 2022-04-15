2cf96fb46d3b636470d7025eaa5df57c72ba2a9f
Ahmed Ayaz <ahmedayaz.business@gmail.com>
2022-04-15 05:55:27 +0500

    Completed Step 00 - Console

--- step-00-console/script.js
+++ step-00-console/script.js
@@ -1,23 +1,37 @@
-console.log("Hello JavaScript !");
-// OUTPUT : 
-// Hello JavaScript !
+// console.log("Hello JavaScript !");
+// // OUTPUT : 
+// // Hello JavaScript !
 
-var name1 = "Ahmed Ayaz";
-console.log("This is" + name1);
-console.log("This is" , name1);
-// OUTPUT :
-// This isAhmed Ayaz
-// This is Ahmed Ayaz
+// var name1 = "Ahmed Ayaz";
+// console.log("This is" + name1);
+// console.log("This is" , name1);
+// // OUTPUT :
+// // This isAhmed Ayaz
+// // This is Ahmed Ayaz
 
-var age = 18;
-console.log("my age is "+ age);
-console.log("my age is ", age);
-// OUTPUT :
-// my age is 18 //(All is White)
-// my age is  18 //(18 is Yellow)
+// var age = 18;
+// console.log("my age is "+ age);
+// console.log("my age is ", age);
+// // OUTPUT :
+// // my age is 18 //(All is White)
+// // my age is  18 //(18 is Yellow)
 
-console.log("Ayaz\tAyaz\nAyaz\tAyaz\nAyaz\tAyaz");
-// OUTPUT :
-// Ayaz    Ayaz
-// Ayaz    Ayaz
-// Ayaz    Ayaz
+// console.log("Ayaz\tAyaz\nAyaz\tAyaz\nAyaz\tAyaz");
+// // OUTPUT :
+// // Ayaz    Ayaz
+// // Ayaz    Ayaz
+// // Ayaz    Ayaz
+
+// var name2 = "Ahmed Ayaz";
+// console.log("My name is " + name2);
+// console.log(`My name is ${name2}`);
+// // OUTPUT :
+// // My name is Ahmed Ayaz
+// // My name is Ahmed Ayaz
+
+// var name3 = "Ahmed Ayaz";
+// console.log(`${name3}\t${name3}\n${name3}\t${name3}\n${name3}\t${name3}`);
+// // OUTPUT :
+// // Ahmed Ayaz      Ahmed Ayaz
+// // Ahmed Ayaz      Ahmed Ayaz
+// // Ahmed Ayaz      Ahmed Ayaz
