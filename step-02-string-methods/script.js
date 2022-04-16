5ae00f7d34502cd702d2e4b084cfcecaaf54293b
Ahmed Ayaz <ahmedayaz.business@gmail.com>
2022-04-17 02:24:21 +0500

    Started Step-02-String-Methods

--- step-02-string-methods/script.js
+++ step-02-string-methods/script.js
@@ -1,2 +1,55 @@
 // https://www.w3schools.com/js/js_string_methods.asp
 // https://www.w3schools.com/js/js_string_search.asp
+
+// let firstName1 = "Ahmed Ayaz";
+// console.log(firstName1.length);
+// // OUTPUT :
+// // 10
+
+// let firstName2 = "Ahmed Ayaz";
+// console.log(firstName2.slice(0 , 2));
+// // OUTPUT :
+// // Ah
+
+// let firstName3 = "Ahmed Ayaz";
+// console.log(firstName3.slice(-4 , -1));
+// // OUTPUT :
+// // Aya
+
+// let firstName4 = "Ahmed Ayaz";
+// console.log(firstName4.slice(7));
+// // OUTPUT :
+// // yaz
+
+// // substring() is similar to slice()
+// // The difference is that substring() cannot accept negative indexes.
+
+// let firstName4 = "Ahmed Ayaz";
+// console.log(firstName4.substring(3 , 8));
+// console.log(firstName4.substring(3));
+// // OUTPUT :
+// // ed Ay
+// // ed Ayaz
+
+// // substr() is similar to slice().
+// // The difference is that the second parameter specifies the length of the extracted part.
+
+// let firstName4 = "Ahmed Ayaz";
+// console.log(firstName4.substr(4 , 4));
+// // OUTPUT :
+// // d Ay
+
+// // The replace() method does not change the string it is called on.
+// // The replace() method returns a new string.
+// // By default, the replace() method is case sensitive.
+
+// let thatName = "Microsoft is my Company";
+// console.log(thatName.replace("Microsoft" , "TechComp"));
+// console.log(thatName);
+// // OUTPUT :
+// // TechComp is my Company
+// // Microsoft is my Company
+
+let a = "Hello";
+let b = "Peter";
+
