99d449274856ad4ee31380228673a8a7f94117af
Ahmed Ayaz <ahmedayaz.business@gmail.com>
2022-04-17 00:26:04 +0500

    Completed Step- 01

--- step-01-variables/script.js
+++ step-01-variables/script.js
@@ -1,40 +1,63 @@
-// https://www.w3schools.com/js/js_variables.asp
-// https://www.w3schools.com/js/js_strings.asp
-// https://www.w3schools.com/js/js_numbers.asp
-// https://www.w3schools.com/js/js_arrays.asp
-// https://www.w3schools.com/js/js_objects.asp
+// // https://www.w3schools.com/js/js_variables.asp
+// // https://www.w3schools.com/js/js_strings.asp
+// // https://www.w3schools.com/js/js_numbers.asp
+// // https://www.w3schools.com/js/js_arrays.asp
+// // https://www.w3schools.com/js/js_objects.asp
 
-var x = 5;
-var y = 6;
-var z = x + y;
-console.log(z);
-// OUTPUT :
-// 11
+// var x = 5;
+// var y = 6;
+// var z = x + y;
+// console.log(z);
+// // OUTPUT :
+// // 11
 
-let x = 5;
-let y = 6;
-let z = x + y;
-console.log(z);
-// OUTPUT :
-// 11
+// let x = 5;
+// let y = 6;
+// let z = x + y;
+// console.log(z);
+// // OUTPUT :
+// // 11
 
-x = 5;
-y = 6;
-z = x + y;
-console.log(z);
-// OUTPUT :
-// 11
+// x = 5;
+// y = 6;
+// z = x + y;
+// console.log(z);
+// // OUTPUT :
+// // 11
 
-var a1 = 10;
-let b1 = a;
-console.log(a1 , b1);
-// OUTPUT :
-// 10   10
+// var a1 = 10;
+// let b1 = a;
+// console.log(a1 , b1);
+// // OUTPUT :
+// // 10   10
 
-var a2 = 10;
-var b2 = a2;
-const c2 = b2 + a2;
-console.log(a2 , b2 , c2);
+// var a2 = 10;
+// var b2 = a2;
+// const c2 = b2 + a2;
+// console.log(a2 , b2 , c2);
 
-// OUTPUT :
-// 10   10  20
+// // OUTPUT :
+// // 10   10  20
+
+// var $ = 1;
+// var _ = 2;
+// console.log($ + _);
+
+// // OUTPUT :
+// // 3
+
+// var $ = 10;
+// var _ = 2;
+// var a = "3";
+// console.log($ + _ , a);
+
+// // OUTPUT :
+// // 12 3
+
+// var $ = 10;
+// var _ = 2;
+// var a = "3";
+// console.log($ + _ + a);
+
+// // OUTPUT :
+// // 123
